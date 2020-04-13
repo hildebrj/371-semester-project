9589c6b7f98e3a8d85b31c6e1baf98d9f70753e0
Julia <hildebrj@mail.gvsu.edu>
2020-04-13 14:07:16 -0400

    Add ingredient attributes to state

--- src/Home.js
+++ src/Home.js
@@ -24,15 +24,33 @@ class Home extends Component {
   submit = (event) => {
     event.preventDefault();
     console.log(this.state);
-    // Add validation
-    // This is where call to FB goes.
+
+    // Checking fields, border is red if empty
+    let allGood = true;
+    let x;
+    for (x in this.state) {
+      if (this.state[x] === "") {
+        document.getElementById(x).style.borderColor = 'red';
+        allGood = false;
+      } else if (x !== "recipeNumIngredients") {
+        document.getElementById(x).style.borderColor = 'grey';
+      }
+    }
+
+    console.log("All good!!!");
+    console.log(allGood);
+
+    // Call to database
+    if (allGood) {
+      // This is where call to DB goes.
+    }
   }
 
   /** Sets the input of the form to the state as it changes */
   handleInputChange = (event) => {
     event.preventDefault();
     this.setState({
-      [event.target.name]: event.target.value
+      [event.target.id]: event.target.value
     });
   }
 
@@ -47,10 +65,20 @@ class Home extends Component {
         recipeNumIngredients: this.state.recipeNumIngredients + 1
       });
       numVal += 1;
+      this.setState({
+        ["recipeIngNum" + numVal]: 1
+      });
+      this.setState({
+        ["ingredientMeasure" + numVal]: ""
+      });
+      this.setState({
+        ["recipeIngredient" + numVal]: ""
+      });
     } else {
       this.setState({
         recipeNumIngredients: this.state.recipeNumIngredients - 1
       });
+      //TODO remove attributes from state here?
       numVal -= 1;
     }
 
@@ -100,80 +128,80 @@ class Home extends Component {
             <h3>Create a new recipe!</h3>
 
             <label>Recipe name: </label>
-            <input type='text' maxLength='40' placeholder='Recipe Name' name='recipeName' onChange={this.handleInputChange} className='normalIn' />
+            <input type='text' maxLength='40' placeholder='Recipe Name' id='recipeName' onChange={this.handleInputChange} className='normalIn' />
 
             <label>Short description: </label>
-            <textarea type='text' maxLength='100' placeholder='Description' name='recipeDescription' onChange={this.handleInputChange} />
+            <textarea type='text' maxLength='100' placeholder='Description' id='recipeDescription' onChange={this.handleInputChange} />
 
             <label>Number of servings: </label>
-            <input type='number' min='1' max='25' defaultValue='1' name='recipeServings' onChange={this.handleInputChange} className='normalIn' />
+            <input type='number' min='1' max='25' defaultValue='1' id='recipeServings' onChange={this.handleInputChange} className='normalIn' />
 
             <label>Preparation time (mins): </label>
-            <input type='number' min='1' max='180' defaultValue='10' name='recipePrepTime' onChange={this.handleInputChange} className='normalIn' />
+            <input type='number' min='1' max='180' defaultValue='10' id='recipePrepTime' onChange={this.handleInputChange} className='normalIn' />
 
             <label>Cook time (mins): </label>
-            <input type='number' min='1' max='240' defaultValue='10' name='recipeCookTime' onChange={this.handleInputChange} className='normalIn' />
+            <input type='number' min='1' max='240' defaultValue='10' id='recipeCookTime' onChange={this.handleInputChange} className='normalIn' />
 
             <label>List of ingredients: </label>
             <div className='ingredientRow'>
-              <input type='number' min='0' max='10' defaultValue='1' name='recipeIngNum1' onChange={this.handleInputChange} className='shortIn' />
-              <input type='text' maxLength='10' placeholder='Measure' name='ingredientMeasure1' onChange={this.handleInputChange} className='medIn'/>
-              <input type='text' maxLength='40' placeholder='Ingredient' name='recipeIngredient1' onChange={this.handleInputChange} className='normalIn' />
+              <input type='number' min='0' max='10' defaultValue='1' id='recipeIngNum1' onChange={this.handleInputChange} className='shortIn' />
+              <input type='text' maxLength='10' placeholder='Measure' id='ingredientMeasure1' onChange={this.handleInputChange} className='medIn' />
+              <input type='text' maxLength='40' placeholder='Ingredient' id='recipeIngredient1' onChange={this.handleInputChange} className='normalIn' />
             </div>
             <div className='ingredientRow'>
-              <input type={this.checkNumNum(2)} min='0' max='10' defaultValue='1' name='recipeIngNum2' onChange={this.handleInputChange} className='shortIn' />
-              <input type={this.checkNumInput(2)} maxLength='10' placeholder='Measure' name='ingredientMeasure2' onChange={this.handleInputChange} className='medIn'/>
-              <input type={this.checkNumInput(2)} maxLength='40' placeholder='Ingredient' name='recipeIngredient2' onChange={this.handleInputChange} className='normalIn' />
+              <input type={this.checkNumNum(2)} min='0' max='10' defaultValue='1' id='recipeIngNum2' onChange={this.handleInputChange} className='shortIn' />
+              <input type={this.checkNumInput(2)} maxLength='10' placeholder='Measure' id='ingredientMeasure2' onChange={this.handleInputChange} className='medIn' />
+              <input type={this.checkNumInput(2)} maxLength='40' placeholder='Ingredient' id='recipeIngredient2' onChange={this.handleInputChange} className='normalIn' />
             </div>
             <div className='ingredientRow'>
-              <input type={this.checkNumNum(3)} min='0' max='10' defaultValue='1' name='recipeIngNum3' onChange={this.handleInputChange} className='shortIn' />
-              <input type={this.checkNumInput(3)} maxLength='10' placeholder='Measure' name='ingredientMeasure3' onChange={this.handleInputChange} className='medIn'/>
-              <input type={this.checkNumInput(3)} maxLength='40' placeholder='Ingredient' name='recipeIngredient3' onChange={this.handleInputChange} className='normalIn' />
+              <input type={this.checkNumNum(3)} min='0' max='10' defaultValue='1' id='recipeIngNum3' onChange={this.handleInputChange} className='shortIn' />
+              <input type={this.checkNumInput(3)} maxLength='10' placeholder='Measure' id='ingredientMeasure3' onChange={this.handleInputChange} className='medIn' />
+              <input type={this.checkNumInput(3)} maxLength='40' placeholder='Ingredient' id='recipeIngredient3' onChange={this.handleInputChange} className='normalIn' />
             </div>
             <div className='ingredientRow'>
-              <input type={this.checkNumNum(4)} min='0' max='10' defaultValue='1' name='recipeIngNum4' onChange={this.handleInputChange} className='shortIn' />
-              <input type={this.checkNumInput(4)} maxLength='10' placeholder='Measure' name='ingredientMeasure4' onChange={this.handleInputChange} className='medIn'/>
-              <input type={this.checkNumInput(4)} maxLength='40' placeholder='Ingredient' name='recipeIngredient4' onChange={this.handleInputChange} className='normalIn' />
+              <input type={this.checkNumNum(4)} min='0' max='10' defaultValue='1' id='recipeIngNum4' onChange={this.handleInputChange} className='shortIn' />
+              <input type={this.checkNumInput(4)} maxLength='10' placeholder='Measure' id='ingredientMeasure4' onChange={this.handleInputChange} className='medIn' />
+              <input type={this.checkNumInput(4)} maxLength='40' placeholder='Ingredient' id='recipeIngredient4' onChange={this.handleInputChange} className='normalIn' />
             </div>
             <div className='ingredientRow'>
-              <input type={this.checkNumNum(5)} min='0' max='10' defaultValue='1' name='recipeIngNum5' onChange={this.handleInputChange} className='shortIn' />
-              <input type={this.checkNumInput(5)} maxLength='10' placeholder='Measure' name='ingredientMeasure5' onChange={this.handleInputChange} className='medIn'/>
-              <input type={this.checkNumInput(5)} maxLength='40' placeholder='Ingredient' name='recipeIngredient5' onChange={this.handleInputChange} className='normalIn' />
+              <input type={this.checkNumNum(5)} min='0' max='10' defaultValue='1' id='recipeIngNum5' onChange={this.handleInputChange} className='shortIn' />
+              <input type={this.checkNumInput(5)} maxLength='10' placeholder='Measure' id='ingredientMeasure5' onChange={this.handleInputChange} className='medIn' />
+              <input type={this.checkNumInput(5)} maxLength='40' placeholder='Ingredient' id='recipeIngredient5' onChange={this.handleInputChange} className='normalIn' />
             </div>
             <div className='ingredientRow'>
-              <input type={this.checkNumNum(6)} min='0' max='10' defaultValue='1' name='recipeIngNum6' onChange={this.handleInputChange} className='shortIn' />
-              <input type={this.checkNumInput(6)} maxLength='10' placeholder='Measure' name='ingredientMeasure6' onChange={this.handleInputChange} className='medIn'/>
-              <input type={this.checkNumInput(6)} maxLength='40' placeholder='Ingredient' name='recipeIngredient6' onChange={this.handleInputChange} className='normalIn' />
+              <input type={this.checkNumNum(6)} min='0' max='10' defaultValue='1' id='recipeIngNum6' onChange={this.handleInputChange} className='shortIn' />
+              <input type={this.checkNumInput(6)} maxLength='10' placeholder='Measure' id='ingredientMeasure6' onChange={this.handleInputChange} className='medIn' />
+              <input type={this.checkNumInput(6)} maxLength='40' placeholder='Ingredient' id='recipeIngredient6' onChange={this.handleInputChange} className='normalIn' />
             </div>
             <div className='ingredientRow'>
-              <input type={this.checkNumNum(7)} min='0' max='10' defaultValue='1' name='recipeIngNum7' onChange={this.handleInputChange} className='shortIn' />
-              <input type={this.checkNumInput(7)} maxLength='10' placeholder='Measure' name='ingredientMeasure7' onChange={this.handleInputChange} className='medIn'/>
-              <input type={this.checkNumInput(7)} maxLength='40' placeholder='Ingredient' name='recipeIngredient7' onChange={this.handleInputChange} className='normalIn' />
+              <input type={this.checkNumNum(7)} min='0' max='10' defaultValue='1' id='recipeIngNum7' onChange={this.handleInputChange} className='shortIn' />
+              <input type={this.checkNumInput(7)} maxLength='10' placeholder='Measure' id='ingredientMeasure7' onChange={this.handleInputChange} className='medIn' />
+              <input type={this.checkNumInput(7)} maxLength='40' placeholder='Ingredient' id='recipeIngredient7' onChange={this.handleInputChange} className='normalIn' />
             </div>
             <div className='ingredientRow'>
-              <input type={this.checkNumNum(8)} min='0' max='10' defaultValue='1' name='recipeIngNum8' onChange={this.handleInputChange} className='shortIn' />
-              <input type={this.checkNumInput(8)} maxLength='10' placeholder='Measure' name='ingredientMeasure8' onChange={this.handleInputChange} className='medIn'/>
-              <input type={this.checkNumInput(8)} maxLength='40' placeholder='Ingredient' name='recipeIngredient8' onChange={this.handleInputChange} className='normalIn' />
+              <input type={this.checkNumNum(8)} min='0' max='10' defaultValue='1' id='recipeIngNum8' onChange={this.handleInputChange} className='shortIn' />
+              <input type={this.checkNumInput(8)} maxLength='10' placeholder='Measure' id='ingredientMeasure8' onChange={this.handleInputChange} className='medIn' />
+              <input type={this.checkNumInput(8)} maxLength='40' placeholder='Ingredient' id='recipeIngredient8' onChange={this.handleInputChange} className='normalIn' />
             </div>
             <div className='ingredientRow'>
-              <input type={this.checkNumNum(9)} min='0' max='10' defaultValue='1' name='recipeIngNum9' onChange={this.handleInputChange} className='shortIn' />
-              <input type={this.checkNumInput(9)} maxLength='10' placeholder='Measure' name='ingredientMeasure9' onChange={this.handleInputChange} className='medIn'/>
-              <input type={this.checkNumInput(9)} maxLength='40' placeholder='Ingredient' name='recipeIngredient9' onChange={this.handleInputChange} className='normalIn' />
+              <input type={this.checkNumNum(9)} min='0' max='10' defaultValue='1' id='recipeIngNum9' onChange={this.handleInputChange} className='shortIn' />
+              <input type={this.checkNumInput(9)} maxLength='10' placeholder='Measure' id='ingredientMeasure9' onChange={this.handleInputChange} className='medIn' />
+              <input type={this.checkNumInput(9)} maxLength='40' placeholder='Ingredient' id='recipeIngredient9' onChange={this.handleInputChange} className='normalIn' />
             </div>
             <div className='ingredientRow'>
-              <input type={this.checkNumNum(10)} min='0' max='10' defaultValue='1' name='recipeIngNum10' onChange={this.handleInputChange} className='shortIn' />
-              <input type={this.checkNumInput(10)} maxLength='10' placeholder='Measure' name='ingredientMeasure10' onChange={this.handleInputChange} className='medIn'/>
-              <input type={this.checkNumInput(10)} maxLength='40' placeholder='Ingredient' name='recipeIngredient10' onChange={this.handleInputChange} className='normalIn' />
+              <input type={this.checkNumNum(10)} min='0' max='10' defaultValue='1' id='recipeIngNum10' onChange={this.handleInputChange} className='shortIn' />
+              <input type={this.checkNumInput(10)} maxLength='10' placeholder='Measure' id='ingredientMeasure10' onChange={this.handleInputChange} className='medIn' />
+              <input type={this.checkNumInput(10)} maxLength='40' placeholder='Ingredient' id='recipeIngredient10' onChange={this.handleInputChange} className='normalIn' />
             </div>
 
             <button type='button' onClick={this.ingredientBox} id='addButton'>Add Ingredient</button>
             <button type='button' onClick={this.ingredientBox} id='removeButton' style={{ visibility: 'hidden' }}>Remove Ingredient</button>
 
             <label>Directions: </label>
-            <textarea type='text' maxLength='400' placeholder='Directions' name='recipeDirections' onChange={this.handleInputChange} />
+            <textarea type='text' maxLength='400' placeholder='Directions' id='recipeDirections' onChange={this.handleInputChange} />
 
             <label>Food category: </label>
-            <select name='recipeCategory' onChange={this.handleInputChange}>
+            <select id='recipeCategory' onChange={this.handleInputChange}>
               <option value='breakfast'>Breakfast</option>
               <option value='soupsalad'>Soup or Salad</option>
               <option value='mainentree'>Main Entree</option>
@@ -181,7 +209,7 @@ class Home extends Component {
               <option value='dessert'>Dessert</option>
             </select>
 
-            <button type='submit' name='recipeSubmit'>Submit Recipe</button>
+            <button type='submit' id='recipeSubmit'>Submit Recipe</button>
           </form>
 
         </div>
