1ed351a4aa394d0175489c168c122ef606b1d04f
Julia <hildebrj@mail.gvsu.edu>
2020-04-19 14:31:20 -0400

    Initial form check

--- src/Home.test.js
+++ src/Home.test.js
@@ -1,15 +1,45 @@
 import React from 'react';
 //import { render } from '@testing-library/react';
 import Home from './Home';
-import { shallow } from 'enzyme';
+import Form from './Home';
+import { shallow, mount } from 'enzyme';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Home />', () => {
-  it('renders three <Foo /> components', () => {
+  it('initial form fields are empty', () => {
     const wrapper = shallow(<Home />);
-    expect(wrapper.find('.recipeName').length).toBe(0);
+  
+    expect(wrapper.state('recipeName').length).toBe(0);
+    expect(wrapper.state('recipeDescription').length).toBe(0);
+    expect(wrapper.state('recipeServings').length).toBe(1);
+    expect(wrapper.state('recipePrepTime').length).toBe(2);
+    expect(wrapper.state('recipeCookTime').length).toBe(2);
+    expect(wrapper.state('recipeIngNum1')).toBe(1);
+    expect(wrapper.state('ingredientMeasure1').length).toBe(0);
+    expect(wrapper.state('recipeIngredient1').length).toBe(0);
+    expect(wrapper.state('recipeDirections').length).toBe(0);
+    expect(wrapper.state('recipeCategory')).toBe('breakfast');
+    expect(wrapper.state('recipeNumIngredients')).toBe(1);
+  });
+
+  it('fill out fields', () => {
+    const component = mount(<Home />);
+    component.setState({
+      recipeName: "",
+      recipeDescription: "",
+      recipeServings: "1",
+      recipePrepTime: "10",
+      recipeCookTime: "10",
+      recipeIngNum1: 1,
+      ingredientMeasure1: "",
+      recipeIngredient1: "",
+      recipeDirections: "",
+      recipeCategory: "breakfast",
+      recipeNumIngredients: 1
+    });
+
   });
 });
